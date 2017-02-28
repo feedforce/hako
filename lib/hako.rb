fbc51742329c235bddbce37b5714394340af4b92
tsub <t-takayama@feedforce.jp>
2017-02-28 12:26:08 +0900

    Use json logger for docker log-driver

--- lib/hako.rb
+++ lib/hako.rb
@@ -9,6 +9,9 @@ module Hako
         $stdout.sync = true
         Logger.new($stdout).tap do |l|
           l.level = Logger::INFO
+          l.formatter = proc do |severity, datetime, progname, message|
+            %({"severity":"#{severity}","datetime":"#{datetime}","progname":"#{progname}","message":"#{message}"})
+          end
         end
       end
   end
