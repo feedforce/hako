3d427d85c83347bd1c1589f1db0cd255d32f7fa4
tsub <t-takayama@feedforce.jp>
2017-02-28 10:25:14 +0900

    Revert "Enable Task Placement Constraints"
    This reverts commit af709e09c76dbd989e397790c839406ebeac78b6.

--- lib/hako/schedulers/ecs.rb
+++ lib/hako/schedulers/ecs.rb
@@ -481,11 +481,6 @@ module Hako
           },
           count: 1,
           started_by: 'hako oneshot',
-          placement_constraints: [
-            {
-              type: 'distinctInstance',
-            },
-          ],
         )
         result.failures.each do |failure|
           Hako.logger.error("#{failure.arn} #{failure.reason}")
